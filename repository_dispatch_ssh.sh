5333c9414233767bf7e8a526749545dbb563b1e5
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-11-13 11:19:43 +0900

    Added comment about repository_dispatch event

--- repository_dispatch_ssh.sh
+++ repository_dispatch_ssh.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# https://help.github.com/en/actions/automating-your-workflow-with-github-actions/events-that-trigger-workflows#external-events-repository_dispatch
+# https://developer.github.com/v3/repos/#create-a-repository-dispatch-event
+
 if [ -z "${GITHUB_TOKEN}" ]; then
   echo "GITHUB_TOKEN is required"
   exit 1
